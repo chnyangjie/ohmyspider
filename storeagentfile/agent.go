62b68799f98abd58f0a3ed5a289bee874580cd8a
yangjie <yangjie@xueqiu.com>
2023-06-22 02:48:11 +0800

    file agent

--- storeagentfile/agent.go
+++ storeagentfile/agent.go
@@ -14,34 +14,45 @@ type StoreAgentFile struct {
 }
 
 func NewStoreAgentFile(agentId string, filePath string) *StoreAgentFile {
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Printf("Open file error: %v", err)
-		return nil
+	var f *os.File
+	if filePath != "" {
+		f, _ = os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0644)
 	}
 	return &StoreAgentFile{
 		agentId:  agentId,
 		filePath: filePath,
-		file:     file,
+		file:     f,
 	}
 }
 
 func (a *StoreAgentFile) CanStore(req webpipeline.StoreRequest) bool {
-	if len(req.StoreParams) != 1 {
+	if len(req.StoreParams) != len(req.StoreContent) && a.file == nil {
+		log.Printf("StoreAgentFile: StoreParams and StoreContent length not equal, and file is nil")
 		return false
 	}
 	for _, content := range req.StoreContent {
-		_, ok := content.(string)
+		_, ok := content.([]byte)
 		if !ok {
+			log.Printf("StoreAgentFile: StoreContent is not []byte")
 			return false
 		}
 	}
 	return true
 }
 func (a *StoreAgentFile) DoStore(req webpipeline.StoreRequest) error {
-	for _, content := range req.StoreContent {
-		data := content.(string)
-		a.file.Write([]byte(data))
+	if len(req.StoreParams) == len(req.StoreContent) {
+		for i := 0; i < len(req.StoreParams); i++ {
+			data := req.StoreContent[i].([]byte)
+			file, _ := os.OpenFile(req.StoreParams[i], os.O_CREATE|os.O_WRONLY, 0644)
+			defer file.Close()
+			file.Write(data)
+		}
+		return nil
+	} else if a.file != nil {
+		for _, content := range req.StoreContent {
+			data := content.(string)
+			a.file.Write([]byte(data))
+		}
 	}
 	return nil
 }
@@ -49,7 +60,9 @@ func (a *StoreAgentFile) AgentId() string {
 	return a.agentId
 }
 func (a *StoreAgentFile) Close() error {
-	a.file.Close()
+	if a.file != nil {
+		a.file.Close()
+	}
 	return nil
 }
 func (a *StoreAgentFile) UniqFunc() webpipeline.IsUniqFunction {
